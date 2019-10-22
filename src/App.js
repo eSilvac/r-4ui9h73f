ef1207a8673fe7ba913a4ef3e7ab9b8def412337
esilvac <esilva1995@gmail.com>
2019-10-22 13:33:37 -0500

    Solve resolver

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.mirror = this.mirror.bind(this);
+
+    this.state = {
+      mirror: ""
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.mirror} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.mirror}</p>
       </div>
     );
+  }
+
+  mirror(event) {
+    this.setState({
+      mirror: event.target.value
+    });
   }
 }
 
